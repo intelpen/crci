b6f5251ba2264496d06da8d9af106014196a44a9
intelpen <adrian.balan@gmail.com>
2020-01-03 19:59:42 +0100

    started chapter 5 bitoperations, implemented just operations

--- c5_bit_manipulation/5_1_insertion.py
+++ c5_bit_manipulation/5_1_insertion.py
@@ -8,3 +8,24 @@
 # Output: N = 10001001100
 # Hints: #137, #769, #215
 
+#Solution - injection here means that M overrides a part of N, with eventually filling with 0s if needed
+
+from c5_bit_manipulation.general_bit_manipulation_in_python import binary
+def insertion(n: int, m:int, i:int, j:int) -> int:
+    int_len = 32
+    length_insertion = j-i
+    length_m = m.bit_length()
+    mask_right = (1 << i) -1 # create 00000111 , with i of 1
+    mask_left = ((-1) <<j) # create 110000000, with j of zeros
+    mask = mask_left & mask_right
+    delete_j_to_i = n & mask
+    m = m << (j-length_m+1) #moved m in the right position
+    return n | m
+
+n = 0b10000000000
+m = 0b10011
+print(f"n:{binary(n)}")
+print(f"m:{binary(m)}")
+k = insertion(n,m, 2,6)
+print(f'k:{binary(k)}')
+
